82b6dcae8cac19e88175c1439e304fa3b9fef87e
doubleInc <aalavi@protonmail.com>
2020-04-20 17:45:24 +1000

    add remde and comments

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { database } from "../../../firebase";
 import ReactDOM from "react-dom";
 
 export default class Welcome extends React.Component {
+  state = {
+    data: null,
+  };
+
+  // component update before loading dom
+  componentDidMount = async () => {
+    // listen for any change("/" => root) in the db and return a snapshot
+    const starCountRef = await database.ref("/").on("value", (snapshot) => {
+      // everytime a change occurs in the db, it will be logged to console
+      console.log("Data changed", snapshot.val());
+    });
+
+    console.log(starCountRef);
+  };
+
   render() {
     return <h1>Hello world</h1>;
   }
