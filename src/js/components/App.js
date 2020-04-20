d9f81ccdd4d248d2ee030bf6d249ebe16a6a938f
doubleInc <aalavi@protonmail.com>
2020-04-20 19:10:33 +1000

    Add to the database

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -5,6 +5,20 @@ import ReactDOM from "react-dom";
 export default class Welcome extends React.Component {
   state = {
     data: null,
+    newData: "",
+  };
+
+  handleChange = (event) => {
+    const new_data = event.target.value;
+    this.setState({
+      newData: new_data,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // add or update "new greeting" key in db
+    database.ref().child("New Greeting").set(this.state.newData);
   };
 
   // component update before loading dom
@@ -13,12 +27,28 @@ export default class Welcome extends React.Component {
     const starCountRef = await database.ref("/").on("value", (snapshot) => {
       // everytime a change occurs in the db, it will be logged to console
       console.log("Data changed", snapshot.val());
+
+      this.setState({
+        data: snapshot.val(),
+      });
     });
 
     console.log(starCountRef);
   };
 
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <h1>{JSON.stringify(this.state.data, null, 2)} world</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.newData}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
   }
 }
